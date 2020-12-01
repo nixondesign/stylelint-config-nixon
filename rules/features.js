d88a6f5ce52b7fde4116c0ea8325973d02dc651f
Ryan Pascoe <ryan@nixondesign.com>
2020-12-01 14:18:46 +0000

    Fix deprecated blacklist/whitelist rules

--- rules/features.js
+++ rules/features.js
@@ -4,11 +4,11 @@ module.exports = {
 	'color-no-hex': null,
 
 	// Function
-	'function-blacklist': null,
+	'function-disallowed-list': null,
 	'function-url-no-scheme-relative': true,
-	'function-url-scheme-blacklist': null,
-	'function-url-scheme-whitelist': null,
-	'function-whitelist': null,
+	'function-url-scheme-disallowed-list': null,
+	'function-url-scheme-allowed-list': null,
+	'function-allowed-list': null,
 
 	// Number
 	'number-max-precision': null,
@@ -17,8 +17,8 @@ module.exports = {
 	'time-min-milliseconds': null,
 
 	// Unit
-	'unit-blacklist': null,
-	'unit-whitelist': null,
+	'unit-allowed-list': null,
+	'unit-disallowed-list': null,
 
 	// Value
 	'value-no-vendor-prefix': true,
@@ -27,23 +27,23 @@ module.exports = {
 	'custom-property-pattern': null,
 
 	// Property
-	'property-blacklist': null,
+	'property-disallowed-list': null,
 	'property-no-vendor-prefix': true,
-	'property-whitelist': null,
+	'property-allowed-list': null,
 
 	// Declaration
 	'declaration-no-important': true,
-	'declaration-property-unit-blacklist': null,
-	'declaration-property-unit-whitelist': null,
-	'declaration-property-value-blacklist': null,
-	'declaration-property-value-whitelist': null,
+	'declaration-property-unit-disallowed-list': null,
+	'declaration-property-unit-allowed-list': null,
+	'declaration-property-value-disallowed-list': null,
+	'declaration-property-value-allowed-list': null,
 
 	// Declaration block
 	'declaration-block-single-line-max-declarations': 1,
 
 	// Selector
-	'selector-attribute-operator-blacklist': null,
-	'selector-attribute-operator-whitelist': null,
+	'selector-attribute-operator-disallowed-list': null,
+	'selector-attribute-operator-allowed-list': null,
 	'selector-class-pattern': null,
 	'selector-id-pattern': null,
 	'selector-max-attribute': null,
@@ -60,22 +60,22 @@ module.exports = {
 		ignore: ['attribute']
 	}],
 	'selector-no-vendor-prefix': true,
-	'selector-pseudo-class-blacklist': null,
-	'selector-pseudo-class-whitelist': null,
+	'selector-pseudo-class-disallowed-list': null,
+	'selector-pseudo-class-allowed-list': null,
 
 	// Media feature
-	'media-feature-name-blacklist': null,
+	'media-feature-name-disallowed-list': null,
 	'media-feature-name-no-vendor-prefix': true,
-	'media-feature-name-whitelist': null,
+	'media-feature-name-allowed-list': null,
 
 	// Custom media
 	'custom-media-pattern': null,
 
 	// at-rule
-	'at-rule-blacklist': ['extend'],
+	'at-rule-disallowed-list': ['extend'],
 	'at-rule-no-vendor-prefix': true,
-	'at-rule-whitelist': null,
+	'at-rule-allowed-list': null,
 
 	// Comment
-	'comment-word-blacklist': null
+	'comment-word-disallowed-list': null
 };
